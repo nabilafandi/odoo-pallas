08859584c443251832f7a0a7caa0916824a640be
nabil <nabilafandih@gmail.com>
2025-06-30 23:16:17 +0700

    feat(pallas pos):payment pos date group

---
+++ custom-pallas/pallas_pos/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Pallas POS',
+    'author': 'Pallas',
+    'category': 'POS',
+    'version': '1.0.0',
+    'description': """Pallas POS""",
+    'summary': '',
+    'sequence': 11,
+    'website': '',
+    'depends': ['point_of_sale'],
+    'license': 'LGPL-3',
+    'data': [
+        'views/report_pos_order.xml',
+        'views/pos_payment.xml',
+    ],
+}
